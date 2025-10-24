56e9ce4333deb610a61405bf3a842bcda80af41a
benjamincenturion <benjaminezequielcenturion@gmail.com>
2025-10-24 07:09:53 -0300

    Add FAB (floating action button) to trigger post modal

---
+++ src/components/FAB.jsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import styles from '@/styles/animations.module.css';
+export default function FAB({ onClick }){
+  return (
+    <button className={styles.fab} aria-label="Crear publicación" onClick={onClick}>
+      <svg viewBox="0 0 24 24" width="24" height="24" aria-hidden="true">
+        <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+      </svg>
+    </button>
+  );
+}
