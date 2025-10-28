8d03d2cb1b141ec88c6acaed1fc91157c11075c5
nicorodriguez <nicolasrdz.05@gmail.com>
2025-10-28 00:33:56 -0300

    feat(register): the form is connected to the backend

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,29 +1,9 @@
 // guardar los valores de todos los campos del formulario.
 "use client";
-import {useForm} from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod';
-import { mappedGenders, userSchema } from '@/validations/userSchema';
 import  RegisterForm  from '@/components/RegisterForm';
 
-interface LoginFormData {
-  email: string;
-  password: string;
-}
 
 export default function RegisterPage() {
-  const {register, handleSubmit, reset, formState: {errors}} = useForm({
-    resolver: zodResolver(userSchema)
-  });
-
-  const genderOptions = Object.entries(mappedGenders).map(([key, value]) => (
-    <option value={key} key={key}> {value} </option>
-  ));
-
-  const onSubmit = (data: LoginFormData) => {
-        localStorage.setItem("loginData", JSON.stringify(data));
-        console.log(data)
-        reset()
-    }
   
   return (
     <RegisterForm/>
